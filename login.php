dabe294b01ef505388205abfce230efca49f6d86
David Saah <davesaah@gmail.com>
2024-11-22 15:48:49 +0000

    chore: rename to lowercaps

--- login.php
+++ login.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/login.css">
+    <link rel="stylesheet" href="./css/login.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="ACTIONS/login_action.php" method="post">
+    <form action="./actions/login_action.php" method="post">
 
         <label for="email">Email</label>
         <input type="email" name="email" id="email" placeholder="john.doe@example.com">
@@ -16,8 +16,8 @@
         <input type="password" name="password" id="password" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;">
 
         <input type="submit" id="register" value="Login">
-        
-        <p>Don't have an account? <a href="index.php">Register</a></p>
+
+        <p>Don't have an account? <a href="./index.php">Register</a></p>
     </form>
 </body>
 </html>
